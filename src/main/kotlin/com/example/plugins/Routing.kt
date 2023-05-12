92c6dc2a9a52d289df0664b4c99751c175467d10
Fourcade7 <fourcadeaminov7@gmail.com>
2023-05-12 23:01:21 +0500

    k

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -5,12 +5,38 @@ import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
 import kotlinx.serialization.Serializable
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             //call.respondText("STATUS CODE", status = HttpStatusCode.BadGateway)
             call.respond(User(email = "gale.ramsey@example.com", password = "quot"))
+        }
+
+        get("/filedownload") {
+          val file=File("files/monkey.jpeg")
+          call.response.header(
+              HttpHeaders.ContentDisposition,
+              ContentDisposition.Inline.withParameter(
+                  ContentDisposition.Parameters.FileName,
+                  "file1.jpeg"
+              ).toString()
+          )
+
+          call.respondFile(file)
+        }
+        get("/filedownloadzip") {
+            val file=File("files/ktor base.zip")
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Attachment.withParameter(
+                    ContentDisposition.Parameters.FileName,
+                    "filezip1.zip"
+                ).toString()
+            )
+
+            call.respondFile(file)
         }
     }
 }
